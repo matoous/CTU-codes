dcda179875bd9655c55c7b22d1e815066698936c
Matouš Dzivjak <matoousmaster@gmail.com>
2017-03-06 15:10:26 +0100

    Shortened

--- JAVA/du4/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
+++ JAVA/du4/src/main/java/cz/cvut/fel/pjv/BruteForceAttacker.java
@@ -2,29 +2,25 @@ package cz.cvut.fel.pjv;
 
 public class BruteForceAttacker extends Thief {
     
+    char[] letters = getCharacters();
+    int sp;
+    
     @Override
     public void breakPassword(int sizeOfPassword) {
-        char[] letters = getCharacters();
+        sp = sizeOfPassword;
         char[] did = new char[sizeOfPassword];
-        if(sizeOfPassword == 0){
-            tryOpen(did);
-        }else{
-            int did_len = 0;
+        if(sizeOfPassword == 0) tryOpen(did);
+        else{
             for(int i = 0; i < letters.length; i++){
                 did[0] = letters[i];
-                if(proceedInRecursion(did, did_len + 1, sizeOfPassword-1))break;
+                if(proceedInRecursion(did, did_len + 1)) break;
             }
         }
     }
     
-    private boolean proceedInRecursion(char[] did, int did_len, int todo){
-        if(todo == 0){
-            if(tryOpen(did)){
-                return true;
-            }
-        }
+    private boolean proceedInRecursion(char[] did, int did_len){
+        if(did_len == sp) return tryOpen(did);
         else{
-            char[] letters = getCharacters();
             for (int i = 0; i < letters.length; i++) {
                 did[did_len] = letters[i];
                 if(proceedInRecursion(did, did_len+1, todo-1)) return true;
@@ -32,5 +28,4 @@ public class BruteForceAttacker extends Thief {
         }
         return false;
     }
-    
 }
