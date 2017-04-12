f21975ca703dbe925f750d587b959cf4a3d83b2f
Matouš Dzivjak <matoousmaster@gmail.com>
2017-04-12 18:51:22 +0200

    Update Server.cpp
    CRC checking added

--- PSI/Server.cpp
+++ PSI/Server.cpp
@@ -3,6 +3,28 @@
 #include <stdio.h>
 #include <fstream>
 #include <vector>
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+
+/* CRC-32C (iSCSI) polynomial in reversed bit order. */
+#define POLY 0x82f63b78
+
+/* CRC-32 (Ethernet, ZIP, etc.) polynomial in reversed bit order. */
+/* #define POLY 0xedb88320 */
+
+uint32_t crc32c(uint32_t crc, const char *buf, size_t len)
+{
+	int k;
+
+	crc = ~crc;
+	while (len--) {
+		crc ^= *buf++;
+		for (k = 0; k < 8; k++)
+			crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+	}
+	return ~crc;
+}
 
 #pragma comment(lib, "ws2_32.lib")
 #pragma warning(disable: 4996)
@@ -35,60 +57,72 @@ int _tmain(int argc, _TCHAR* argv[])
 	uint32_t file_size;
 	char file_name[64];
 	char* incomming_bytes;
+	char buffer[4096];
 
 	// loop
 	while (1)
 	{
-		char buffer[4096];
 		ZeroMemory(buffer, sizeof(buffer));
 		if (recvfrom(socketS, buffer, sizeof(buffer), 0, (sockaddr*)&from, &fromlen) != SOCKET_ERROR)
 		{
-			if (strncmp(buffer, "NAME", 4) == 0) {
-				printf("Received NAME packet\n");
-				int l = 0;
-				for (int i = 5; i < 1000; i++) {
-					file_name[l++] = buffer[i];
-					if (buffer[i] == '\0') break;
-				}
+			// Check CRC
+			uint32_t check_crc = 0;
+			for (int n = 4092; n < 4096; n++)
+				check_crc = (check_crc << 8) + (unsigned char)buffer[n];
+			uint32_t crc = 0;
+			crc = crc32c(crc, buffer, 4096 - 4);
+			if (check_crc != crc) {
+				sendto(socketS, "BAD_____________", 16, 0, (sockaddr*)&from, fromlen);
+				continue;
 			}
-			else if (strncmp(buffer, "SIZE", 4) == 0) {
-				printf("Received SIZE packet\n");
-				file_size = 0;
-				int i = 5;
-				while (1) {
-					if (buffer[i] == '\0') break;
-					file_size *= 10;
-					file_size += buffer[i++] - '0';
-				}
-			}
-			else if (strncmp(buffer, "START", 5) == 0) {
-				printf("Received START packet\n");
-				incomming_bytes = (char*)malloc(file_size*sizeof(char));
-			}
-			else if (strncmp(buffer, "DATA", 4) == 0) {
-				uint32_t offset = 0;
-				for (int n = 0; n < 4; n++)
-					offset = (offset << 8) + (unsigned char)buffer[n + 4];
-				if (offset + 4096 > file_size) {
-					for (int i = 0; i < file_size - offset; i++) {
-						incomming_bytes[i + offset] = buffer[i+8];
+			else {
+				if (strncmp(buffer, "NAME", 4) == 0) {
+					printf("Received NAME packet\n");
+					int l = 0;
+					for (int i = 5; i < 1000; i++) {
+						file_name[l++] = buffer[i];
+						if (buffer[i] == '\0') break;
 					}
 				}
-				else {
-					for (int i = 0; i < 4096 - 8; i++) {
-						incomming_bytes[i + offset] = buffer[i+8];
+				else if (strncmp(buffer, "SIZE", 4) == 0) {
+					printf("Received SIZE packet\n");
+					file_size = 0;
+					int i = 5;
+					while (1) {
+						if (buffer[i] == '\0') break;
+						file_size *= 10;
+						file_size += buffer[i++] - '0';
 					}
 				}
-				printf("Received DATA packet with offset: %u\n", offset);
+				else if (strncmp(buffer, "START", 5) == 0) {
+					printf("Received START packet\n");
+					incomming_bytes = (char*)malloc(file_size * sizeof(char));
+				}
+				else if (strncmp(buffer, "DATA", 4) == 0) {
+					uint32_t offset = 0;
+					for (int n = 0; n < 4; n++)
+						offset = (offset << 8) + (unsigned char)buffer[n + 4];
+					if (offset + 4096 > file_size) {
+						for (int i = 0; i < file_size - offset; i++) {
+							incomming_bytes[i + offset] = buffer[i + 8];
+						}
+					}
+					else {
+						for (int i = 0; i < 4096 - 12; i++) {
+							incomming_bytes[i + offset] = buffer[i + 8];
+						}
+					}
+					printf("Received DATA packet with offset: %u\n", offset);
+				}
+				else if (strncmp(buffer, "STOP", 4) == 0) {
+					printf("Received STOP packet.\n");
+					printf("Writing file.\n");
+					ofstream outfile(file_name, ofstream::binary);
+					outfile.write(incomming_bytes, file_size);
+					free(incomming_bytes);
+				}
+				sendto(socketS, "OK______________", 16, 0, (sockaddr*)&from, fromlen);
 			}
-			else if (strncmp(buffer, "STOP", 4) == 0) {
-				printf("Received STOP packet.\n");
-				printf("Writing file.");
-				ofstream outfile(file_name, ofstream::binary);
-				outfile.write(incomming_bytes, file_size);
-				free(incomming_bytes);
-			}
-			sendto(socketS, buffer, 8, 0, (sockaddr*)&from, fromlen);
 		}
 	}
 	closesocket(socketS);
