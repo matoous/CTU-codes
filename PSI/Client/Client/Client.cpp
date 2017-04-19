6d153f950e569ed6dfea8f3d8982127ff1989270
Matouš Dzivjak <matoousmaster@gmail.com>
2017-04-20 00:41:18 +0200

    Add files via upload

---
+++ PSI/Client/Client/Client.cpp
@@ -0,0 +1,235 @@
+#include "stdafx.h"
+#include <winsock2.h>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include "md5.h"
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+/* NAMESPACE */
+using namespace std;
+
+
+/* CRC-32C (iSCSI) polynomial in reversed bit order. */
+#define POLY 0x82f63b78
+
+/* CRC-32 (Ethernet, ZIP, etc.) polynomial in reversed bit order. */
+/* #define POLY 0xedb88320 */
+uint32_t crc32c(uint32_t crc, const char *buf, size_t len)
+{
+	int k;
+
+	crc = ~crc;
+	while (len--) {
+		crc ^= *buf++;
+		for (k = 0; k < 8; k++)
+			crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+	}
+	return ~crc;
+}
+/* CRC END */
+
+/* Global variables */
+SOCKET socketC;
+struct sockaddr_in serverInfo;
+int len;
+
+/* Socket init */
+void InitWinsock()
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+}
+
+/* Read all bytes from file */
+static vector<char> ReadAllBytes(const string filename)
+{
+	ifstream ifs(filename, ios::binary | ios::ate);
+	ifstream::pos_type pos = ifs.tellg();
+
+	vector<char>  result(pos);
+	char* result2 = (char*)malloc(pos * sizeof(char));
+
+	ifs.seekg(0, ios::beg);
+	ifs.read(&result[0], pos);
+
+	ifs.seekg(0, ios::beg);
+	ifs.read(result2, pos);
+
+	return result;
+}
+
+/* convert int to bytes */
+vector<unsigned char> intToBytes(uint32_t paramInt)
+{
+	vector<unsigned char> arrayOfByte(4);
+	for (int i = 0; i < 4; i++)
+		arrayOfByte[3 - i] = (paramInt >> (i * 8));
+	return arrayOfByte;
+}
+
+/* send string */
+bool sendBuffer(const char* s) {
+	// init variables
+	int tries = 0;
+	char back_buff[16];
+	char buffer[4096];
+	ZeroMemory(buffer, sizeof(buffer));
+
+	// print string to buffer
+	sprintf(buffer, s);
+	
+	// compute crc
+	uint32_t crc = 0;
+	crc = crc32c(crc, buffer, 4092);
+	vector<unsigned char> crc_bytes = intToBytes(crc);
+	for (int i = 4092; i < 4096; i++) {
+		buffer[i] = crc_bytes[i-4092];
+	}
+
+	// try sanding, max five times
+	do {
+		tries++;
+		if (sendto(socketC, buffer, sizeof(buffer), 0, (sockaddr*)&serverInfo, len) != SOCKET_ERROR)
+		{
+			if (recvfrom(socketC, back_buff, sizeof(back_buff), 0, (sockaddr*)&serverInfo, &len) != SOCKET_ERROR)
+			{
+				back_buff[15] = '\0';
+				printf("Receive response from server: %s\n", back_buff);
+			}
+		}
+	} while (strncmp(back_buff, "OK", 2) != 0 && tries != 8);
+
+	// return
+	return tries < 8;
+}
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	// setup
+	InitWinsock();
+	len = sizeof(serverInfo);
+	serverInfo.sin_family = AF_INET;
+	serverInfo.sin_port = htons(4000);
+	serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");                                    // CHANGE THIS
+	string file = "D:\\in.txt";                                                             // CHANGE THIS
+	string fname = "D:\\out.txt";                                                           // CHANGE THIS
+
+
+	// open socket
+	socketC = socket(AF_INET, SOCK_DGRAM, 0);
+
+	// file md5
+	std::ifstream t(file);
+	std::string str2;
+
+	t.seekg(0, std::ios::end);
+	str2.reserve(t.tellg());
+	t.seekg(0, std::ios::beg);
+
+	str2.assign((std::istreambuf_iterator<char>(t)),
+		std::istreambuf_iterator<char>());
+	cout << str2 << endl;
+	string hashed = md5(str2);
+
+	// read file bytes
+	vector<char> file_bytes = ReadAllBytes(file);
+
+	char str[120];
+
+	// send file name
+	ZeroMemory(str, sizeof(str));
+	sprintf(str, "NAME=%s", fname.c_str());
+	if (!sendBuffer(str)) {
+		cout << "Error sending NAME packet" << endl;
+		return 0;
+	}
+
+	// send file size
+	ZeroMemory(str, sizeof(str));
+	sprintf(str, "SIZE=%ld", file_bytes.size());
+	cout << "file size " << file_bytes.size() << endl;
+	if (!sendBuffer(str)) {
+		cout << "Error sending SIZE packet" << endl;
+		return 0;
+	}
+
+	// send file hash
+	ZeroMemory(str, sizeof(str));
+	sprintf(str, "HASH=");
+	for (int i = 0; i < hashed.size(); i++) {
+		str[i + 5] = hashed[i];
+	}
+	str[5 + hashed.size()] = '\0';
+	cout << "sanding hash " << str << endl;
+	if (!sendBuffer(str)) {
+		cout << "Error sending HASH packet" << endl;
+		return 0;
+	}
+
+	// start file transmit
+	ZeroMemory(str, sizeof(str));
+	sprintf(str, "START");
+	if (!sendBuffer(str)) {
+		cout << "Error sending START packet" << endl;
+		return 0;
+	}
+
+	// transmit
+	uint32_t offset = 0;
+	char buffer2[4096];
+	while (offset < file_bytes.size()) {
+		buffer2[0] = 'D';
+		buffer2[1] = 'A';
+		buffer2[2] = 'T';
+		buffer2[3] = 'A';
+		// offset
+		vector<unsigned char> offset_bytes = intToBytes(offset);
+		for (int i = 0; i < 4; i++) {
+			buffer2[i+4] = offset_bytes[i];
+		}
+		// data
+		for (int i = 0; i < ((4096 - 12 < file_bytes.size() - offset) ? (4096-12) : (file_bytes.size() - offset)); i++) {
+			buffer2[i+8] = file_bytes[i + offset];
+		}
+		// CRC
+		uint32_t crc = 0;
+		crc = crc32c(crc, buffer2, 4092);
+		vector<unsigned char> crc_bytes = intToBytes(crc);
+		for (int i = 4092; i < 4096; i++) {
+			buffer2[i] = crc_bytes[i - 4092];
+		}
+		// send
+		if (sendto(socketC, buffer2, sizeof(buffer2), 0, (sockaddr*)&serverInfo, len) != SOCKET_ERROR)
+		{
+			if (recvfrom(socketC, buffer2, sizeof(buffer2), 0, (sockaddr*)&serverInfo, &len) != SOCKET_ERROR)
+			{
+				//buffer2[15] = '\0';
+				//printf("Receive response from server: %s\n", buffer2);
+			}
+		}
+		offset += 4096 - 12;
+		ZeroMemory(buffer2, sizeof(buffer2));
+	}
+
+	// end file transmit
+	ZeroMemory(str, sizeof(str));
+	sprintf(str, "STOP");
+	if (!sendBuffer(str)) {
+		cout << "Error sending STOP packet" << endl;
+		return 0;
+	}
+
+	// close socket
+	system("pause");
+}
---
+++ PSI/Server/Server/Server.cpp
@@ -0,0 +1,177 @@
+#include "stdafx.h"
+#include <winsock2.h>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <iomanip>
+#include <iostream>
+#include <stddef.h>
+#include <stdint.h>
+#include <Windows.h>
+#include "md5.h"
+
+/* CRC-32C (iSCSI) polynomial in reversed bit order. */
+#define POLY 0x82f63b78
+
+/* CRC-32 (Ethernet, ZIP, etc.) polynomial in reversed bit order. */
+/* #define POLY 0xedb88320 */
+
+uint32_t crc32c(uint32_t crc, const char *buf, size_t len)
+{
+	int k;
+
+	crc = ~crc;
+	while (len--) {
+		crc ^= *buf++;
+		for (k = 0; k < 8; k++)
+			crc = crc & 1 ? (crc >> 1) ^ POLY : crc >> 1;
+	}
+	return ~crc;
+}
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma warning(disable: 4996)
+
+using namespace std;
+
+void InitWinsock(){
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+}
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	// SETUP, do not tuch!
+	SOCKET socketS;
+
+	InitWinsock();
+	struct sockaddr_in local;
+	struct sockaddr_in from;
+	int fromlen = sizeof(from);
+	local.sin_family = AF_INET;
+	local.sin_port = htons(4000);
+	local.sin_addr.s_addr = INADDR_ANY;
+
+	socketS = socket(AF_INET, SOCK_DGRAM, 0);
+	bind(socketS, (sockaddr*)&local, sizeof(local));
+	printf("Listening on port 4000\n");
+
+	// my vars
+	uint32_t file_size;
+	char file_name[64];
+	char* incomming_bytes;
+	char buffer[4096];
+	char file_hash[128];
+	char* re_message;
+	int fails_in_row = 0;
+
+	// loop
+	while (1)
+	{
+		ZeroMemory(buffer, sizeof(buffer));
+		if (recvfrom(socketS, buffer, sizeof(buffer), 0, (sockaddr*)&from, &fromlen) != SOCKET_ERROR)
+		{
+			// Check CRC
+			uint32_t check_crc = 0;
+			for (int n = 4092; n < 4096; n++)
+				check_crc = (check_crc << 8) + (unsigned char)buffer[n];
+			uint32_t crc = 0;
+			crc = crc32c(crc, buffer, 4096 - 4);
+			if (check_crc != crc) {
+				fails_in_row++;
+				if (fails_in_row == 8) {
+					cout << "transition failed" << endl;
+					free(incomming_bytes);
+				}
+				sendto(socketS, "BAD_____________", 16, 0, (sockaddr*)&from, fromlen);
+				continue;
+			}
+			else {
+				fails_in_row = 0;
+				if (strncmp(buffer, "NAME", 4) == 0) {
+					printf("Received NAME packet\n");
+					int l = 0;
+					for (int i = 5; i < 1000; i++) {
+						file_name[l++] = buffer[i];
+						if (buffer[i] == '\0') break;
+					}
+					re_message = "OK______________";
+				}
+				else if (strncmp(buffer, "SIZE", 4) == 0) {
+					printf("Received SIZE packet\n");
+					file_size = 0;
+					int i = 5;
+					while (1) {
+						if (buffer[i] == '\0') break;
+						file_size *= 10;
+						file_size += buffer[i++] - '0';
+					}
+					re_message = "OK______________";
+				}
+				else if (strncmp(buffer, "START", 5) == 0) {
+					printf("Received START packet\n");
+					incomming_bytes = (char*)malloc(file_size * sizeof(char));
+					re_message = "OK______________";
+				}
+				else if (strncmp(buffer, "DATA", 4) == 0) {
+					uint32_t offset = 0;
+					for (int n = 0; n < 4; n++)
+						offset = (offset << 8) + (unsigned char)buffer[n + 4];
+					if (offset + 4096 > file_size) {
+						for (int i = 0; i < file_size - offset; i++) {
+							incomming_bytes[i + offset] = buffer[i + 8];
+						}
+					}
+					else {
+						for (int i = 0; i < 4096 - 12; i++) {
+							incomming_bytes[i + offset] = buffer[i + 8];
+						}
+					}
+					re_message = "OK______________";
+				}
+				else if (strncmp(buffer, "HASH", 4) == 0) {
+					printf("Received HASH packet\n");
+					int l = 0;
+					for (int i = 5; i < 128+5; i++) {
+						file_hash[l++] = buffer[i];
+						if (buffer[i] == '\0') break;
+					}				
+					re_message = "OK______________";
+				}
+				else if (strncmp(buffer, "STOP", 4) == 0) {
+					printf("Received STOP packet.\n");
+					printf("Writing file.\n");
+					ofstream outfile(file_name, ofstream::binary);
+					outfile.write(incomming_bytes, file_size);
+					free(incomming_bytes);
+					outfile.close();
+
+					// file md5
+					ifstream t(file_name);
+					string str2;
+					t.seekg(0, ios::end);
+					str2.reserve(t.tellg());
+					t.seekg(0, ios::beg);
+
+					str2.assign((istreambuf_iterator<char>(t)),
+						istreambuf_iterator<char>());
+					string hashed = md5(str2);
+					string orig_hash(file_hash);
+					if (hashed.compare(orig_hash) == 0) {
+						cout << "file ok" << endl;
+						re_message = "OK______________";
+					}
+					else {
+						cout << "file bad" << endl;
+						re_message = "BF______________";
+					}
+				}
+				sendto(socketS, re_message, 16, 0, (sockaddr*)&from, fromlen);
+			}
+		}
+	}
+	closesocket(socketS);
+	return 0;
+}
+
