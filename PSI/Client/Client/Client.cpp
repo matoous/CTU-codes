211252d4a1f22daaec7d53dafa3a45faa181c7a0
Matouš Dzivjak <matoousmaster@gmail.com>
2017-05-15 19:25:52 +0200

    Update Client.cpp

--- PSI/Client/Client/Client.cpp
+++ PSI/Client/Client/Client.cpp
@@ -1,3 +1,11 @@
+/*******************************************************************
+  APO semestral project by Matous Dzivjak
+  (C) Copyright 2017 - 2017 by Matous Dzivjak
+      e-mail:   dzivjmat@fel.cvut.cz
+      license:  MIT
+      Feel free to copy as long as you wont use it for the homework
+ *******************************************************************/
+
 #include "stdafx.h"
 #include <winsock2.h>
 #include <stdio.h>
