2c89403860b8796ec01611953f6b2563e2d23adc
Matouš Dzivjak <matoousmaster@gmail.com>
2017-05-24 09:43:57 +0200

    Update sliding_window.cpp

--- PSI/Client/Client/sliding_window.cpp
+++ PSI/Client/Client/sliding_window.cpp
@@ -1,3 +1,11 @@
+/*******************************************************************
+project:         PSI semestral project by Matous Dzivjak
+(C) Copyright:   2017 - 2017 by Matous Dzivjak
+e-mail:          dzivjmat@fel.cvut.cz
+license:         MIT
+Feel free to copy as long as you wont use it for the homework
+*******************************************************************/
+
 #include "sliding_window.h"
 
 // Init sliding window with given size
