53f60ff6fc96bec5752e83721b33af414c8af302
Matouš Dzivjak <matoousmaster@gmail.com>
2017-05-26 23:04:01 +0200

    Update rsa.h

--- PSI/Client/Client/rsa.h
+++ PSI/Client/Client/rsa.h
@@ -14,27 +14,6 @@ class Rsa {
 private:
 	bignum_t p, q, n, cnt, totient, e, d, public_e, public_n;
 	bool set;
-	bignum_t gcd(bignum_t a, bignum_t h)
-	{
-		bignum_t temp;
-		while (1) {
-			temp = a%h;
-			if (temp == 0)
-				return h;
-			a = h;
-			h = temp;
-		}
-	}
-	bignum_t modpow(bignum_t base, bignum_t exp, bignum_t modulus) {
-		base %= modulus;
-		bignum_t result = 1;
-		while (exp > 0) {
-			if (exp & 1) result = (result * base) % modulus;
-			base = (base * base) % modulus;
-			exp >>= 1;
-		}
-		return result;
-	}
 public:
 	Rsa(bignum_t a, bignum_t b);
 	void set_public(bignum_t n, bignum_t e);
